e096f729f79d4c241446e4f2c0a4d378f02fd029
Leonardo Zabotto <leozabotto02@gmail.com>
2023-03-18 14:44:25 -0300

    feat: header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
+import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
 
-import { GlobalStyle } from './styles/global'
-import { defaultTheme } from './styles/themes/default'
+import { Router } from './Router';
+
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button></Button>
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
-  )
+  );
 }
