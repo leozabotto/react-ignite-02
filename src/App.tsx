73a97994aae3c6fe229ae147806a678697de50fb
Leonardo Zabotto <leozabotto02@gmail.com>
2023-03-17 22:21:42 -0300

    theme setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import { ThemeProvider } from 'styled-components';
+import { Button } from './components/Button';
+import { defaultTheme } from './styles/themes/default';
+
 export function App() {
-  return <h1>Hello, World!</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button></Button>
+    </ThemeProvider>
+  );
 }
